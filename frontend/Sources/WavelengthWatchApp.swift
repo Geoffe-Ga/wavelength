ac8c8b24d15dfc69f139caf7a0f9309a00f1b97c
Geoff Gallinger <geoffe.gallinger@gmail.com>
2025-08-20 10:17:07 -0700

    Re-initialize correctly as xcodeproj

---
+++ frontend/Sources/WavelengthWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavelengthWatchAppApp.swift
+//  WavelengthWatchApp Watch App
+//
+//  Created by Geoff Gallinger on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WavelengthWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
